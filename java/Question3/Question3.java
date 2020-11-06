ede43feddd77b506e18661079b548bed4e54f186
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-11-06 16:19:38 -0500

    Commited by GCC-Challenge-2020

--- java/Question3/Question3.java
+++ java/Question3/Question3.java
@@ -57,7 +57,7 @@ class Solution {
 
         StringBuilder answer = new StringBuilder(profit.length*3);
         for(int y=0;y<zz.length;y+=2){
-            if (zz[y] = "0"){answer.append("-1");}
+            if (zz[y].equals(0){answer.append("-1");}
             else{
             answer.append(zz[y]);
             answer.append(" ");
@@ -65,7 +65,7 @@ class Solution {
             if (zz.length > 2 && y != zz.length/2){ answer.append(",");}
         }
         String answer2 = answer.toString();
-      //  if (answer2.charAt(0) == '0') {return "-1";}
+
         return answer2;
 
     }
