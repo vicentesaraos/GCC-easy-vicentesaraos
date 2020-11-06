4fc6de760fce3edac076a3bc7c0e33257cd692d7
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-11-06 11:04:16 -0500

    File Created by GCC-Challenge-2020

---
+++ cplusplus/Question2/Question2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findMaxProfit(int numOfPredictedTimes, vector<int> predictedSharePrices) {
+    // Participant's code will go here
+    return -1;
+}
+
+vector<int> splitStringToInt(const string& str, char delim) {
+    vector<int> strings;
+    size_t start;
+    size_t end = 0;
+    while ((start = str.find_first_not_of(delim, end)) != string::npos) {
+        end = str.find(delim, start);
+        strings.push_back(stoi(str.substr(start, end - start)));
+    }
+    return strings;
+}
+
+void printVector(vector<int> vec) {
+    for (vector<int>::const_iterator i = vec.begin(); i != vec.end(); ++i) {
+        cout << *i << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+    string firstLine;
+    getline(cin, firstLine);
+
+    vector<int> firstLineVec = splitStringToInt(firstLine, ' ');
+    int numOfPredictedTimes = firstLineVec[0];
+    vector<int> predictedSharePrices(firstLineVec.begin()+1, firstLineVec.end());
+
+
+    int result = findMaxProfit(numOfPredictedTimes, predictedSharePrices);
+
+    // Do not remove below line
+    cout << result << "\n";
+    // Do not print anything after this line
+
+    return 0;
+}
