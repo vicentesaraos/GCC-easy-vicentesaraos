0ecc7ddece840f525a5858ff88ec11fb593e9f15
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-11-06 11:37:34 -0500

    Commited by GCC-Challenge-2020

--- java/Question1/Question1.java
+++ java/Question1/Question1.java
@@ -6,7 +6,22 @@ class Solution {
 
     // You may change this function parameters
     static int findMaxProfit(int numOfPredictedDays, int[] predictedSharePrices) {
-        return -1;
+      
+      // find greatest positive range of two positive integers
+      // a < b
+      // if a - b >0 && >min
+      // from 0 to len-2
+      //
+      
+      int min=Integer.MAX_VALUE;
+      int max=-1;
+      for(int i =0; i<predictedSharePrices.length; i++){
+        if (min > predictedSharePrices[i]){min = predictedSharePrices[i];}
+        else if (predictedSharePrices[i] - min > max){max = predictedSharePrices[i]-min;}
+              
+      }
+      
+        return max;
     }
 
     private static final Scanner scanner = new Scanner(System.in);
@@ -18,7 +33,10 @@ class Solution {
         int[] predictedSharePrices = Arrays.copyOfRange(firstLineArr, 1, firstLineArr.length);
         scanner.close();
 
+
         int result = findMaxProfit(numOfPredictedDays, predictedSharePrices);
+
+
 
         // Do not remove this line
         System.out.println(result);
