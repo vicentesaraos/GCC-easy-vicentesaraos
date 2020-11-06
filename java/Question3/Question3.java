58ee0701bf78c34c73943d09a02892be66168039
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-11-06 16:42:48 -0500

    Commited by GCC-Challenge-2020

--- java/Question3/Question3.java
+++ java/Question3/Question3.java
@@ -28,7 +28,7 @@ class Solution {
     public static String findMinDays(int[] values, int[] profit) {
 
         // Participant's code will go here
-         int a=0;
+              int a=0;
         int b=0;
         int c=0;
         int aa = 0;
@@ -38,30 +38,31 @@ class Solution {
         int k=0;
         a=profit[k];
 
-            for(int l=0, d=l+1; l<values.length && d<values.length;d++){
-                b=values[l];
-                c=values[d];
+        for(int l=0, d=l+1; l<values.length && d<values.length;d++){
+            
+            b=values[l];
+            c=values[d];
 
-                if(c>b){
-                    if((a==c-b) &&
-                            (d <= z[1]-1) &&
-                            (d-l <= z[1]-1-z[0]-1))
-                    {z[0]=l+1;z[1]=d+1;++l;};}
-                
-                if(d==values.length-1){++l;d=l;}
-                
-                if(l==values.length-1 && d==values.length-1){zz[aa]=z[0];zz[aa+1]=z[1];z[0]=0;z[1]=Integer.MAX_VALUE;
-                    if(k==profit.length-1){break;};aa+=2;++k;l=0;d=l;
-                 {a=profit[k];};}
-            }
+            if(c>b){
+                if((a==c-b) &&
+                        (d <= z[1]-1) &&
+                        (d-l <= z[1]-1-z[0]-1))
+                {z[0]=l+1;z[1]=d+1;};}
+
+            if(d==values.length-1){++l;d=l;}
+            
+            if((l==values.length -1)&&(d==values.length-1)){zz[aa]=z[0];zz[aa+1]=z[1];z[0]=0;z[1]=Integer.MAX_VALUE;
+                if(k==profit.length-1){break;};aa+=2;++k;l=0;d=l;
+                {a=profit[k];};}
+        }
 
         StringBuilder answer = new StringBuilder(profit.length*3);
         for(int y=0;y<zz.length;y+=2){
             if (zz[y]==0){answer.append("-1");}
             else{
-            answer.append(zz[y]);
-            answer.append(" ");
-            answer.append(zz[y+1]);}
+                answer.append(zz[y]);
+                answer.append(" ");
+                answer.append(zz[y+1]);}
             if (zz.length > 2 && y != zz.length/2){ answer.append(",");}
         }
         String answer2 = answer.toString();
